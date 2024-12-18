4f5d5de26462a49e503b2c4cd63608516141e665
dmadangure <donaldmadangure@gmail.com>
2024-12-18 10:05:28 +0200

    all routing done

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css'
+})
+export class PortfolioComponent {
+  constructor(private titleService: Title){
+    this.titleService.setTitle("Donald Madangure - Portfolio")
+  }
+}
